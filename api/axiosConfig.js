84bc706776d3b32924e34611a7dc6134feb63ae1
liaHi19 <gn.natalia19@gmail.com>
2022-10-04 09:38:58 +0300

    [#31] Change base url

--- api/axiosConfig.js
+++ api/axiosConfig.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-import { IS_PRODUCTION, PUBLIC, CLIENT } from "../config/constants";
+// import { IS_PRODUCTION, PUBLIC, CLIENT } from "../config/constants";
 
 export const apiClient = axios.create({
   baseURL: "https://next-basic-57623-default-rtdb.firebaseio.com/events.json",
 });
 
 export const apiBase = axios.create({
-  baseURL: IS_PRODUCTION ? `${PUBLIC}/api` : `${CLIENT}/api`,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
