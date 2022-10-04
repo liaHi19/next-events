e095e1199c4e8a5113d57284cbb1343d7bbd0955
liaHi19 <gn.natalia19@gmail.com>
2022-10-04 09:15:24 +0300

    [#30] Fix implementing MongoDB

--- helpers/mongodb.js
+++ helpers/mongodb.js
@@ -1,23 +1,48 @@
 import { MongoClient } from "mongodb";
 import { IS_PRODUCTION } from "../config/constants";
 
-const MONGO = process.env.MONGODB_URI || process.env.APP_MONGODB_URI;
+const MONGODB_URI_CHECK = IS_PRODUCTION
+  ? process.env.MONGODB_URI
+  : process.env.APP_MONGODB_URI;
+const MONGODB_DB = process.env.MONGODB_NAME;
+
+// check the MongoDB URI
+if (!MONGODB_URI_CHECK) {
+  throw new Error("Define the MONGODB_URI environmental variable");
+}
+
+// check the MongoDB DB
+if (!MONGODB_DB) {
+  throw new Error("Define the MONGODB_DB environmental variable");
+}
+
+let cachedClient = null;
+let cachedDb = null;
 
 export const connectDB = async () => {
-  if (MONGO) {
-    try {
-      const client = await MongoClient.connect(
-        IS_PRODUCTION ? process.env.MONGODB_URI : process.env.APP_MONGODB_URI
-      );
-      const db = await client.db();
-      return { client, db };
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  } else {
-    console.log("without connection");
+  if (cachedClient && cachedDb) {
+    return {
+      client: cachedClient,
+      db: cachedDb,
+    };
   }
+
+  const opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  const client = new MongoClient(MONGODB_URI_CHECK, opts);
+  await client.connect();
+  const db = client.db(MONGODB_DB);
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
 };
 
 export const checkExistingDocument = async (existingDoc, collection) => {
--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     APP_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     APP_MONGODB_URI: process.env.REACT_APP_MONGODB_URI,
     MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_NAME: process.env.MONGODB_NAME,
   },
 };
 
